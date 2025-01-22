9dda944ffa5ec00fbf543db11ea8b8534a083380
shahnoor <113544135+shahnoork109@users.noreply.github.com>
2025-01-22 10:20:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let age = "33abc"
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// 33 --> number
+// "33" --> sting
+// "33abc" --> NaN
+// true = 1,  false = 0     ""= false ,     "anything"= true

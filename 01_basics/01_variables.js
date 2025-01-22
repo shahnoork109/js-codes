2e0c1f2ae7af9f4400df93f9467e4ec6b2ae14a7
shahnoor <113544135+shahnoork109@users.noreply.github.com>
2025-01-22 09:57:37 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "khan070304@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+
+// accountId = 3234
+accountEmail = "helloJii12.com"
+accountPassword = "345fgfg"
+accountCity = "Bengaluru"
+let accountState;
+
+/*
+perefer not to use var
+bacause of issue in block scpe and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 3)     //we are using node.js not browser
+
+let name = "shahnor"
+let age = 20
+let istrue = true
+
+
+
+//number = 2 power 53
+//bigint
+//sting   ""
+//boolean true/false
+//null
+//undefined
+
+
+console.log(typeof age);
+

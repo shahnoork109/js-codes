787aa8e2bbaba04b53514ea8fe30e86841157c5b
shahnoor <113544135+shahnoork109@users.noreply.github.com>
2025-01-22 14:08:11 +0000

    conversion and conversion operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,35 @@
 let age = "33abc"
 
-console.log(typeof age);
-console.log(typeof(age));
+// console.log(typeof age);
+// console.log(typeof(age));
 
 let valueInNumber = Number(age)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // 33 --> number
 // "33" --> sting
 // "33abc" --> NaN
 // true = 1,  false = 0     ""= false ,     "anything"= true
+
+
+
+//  ********************* operations ****************************
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = " World"
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log(1 + 2);           //3
+console.log("1" + 2);         //12
+console.log(1 + "2");         //12
+console.log("1" + 2 + 3);     //123  
+console.log(1 + 2 + "3");     //33
+
+

cde129d11cb8b7034b5cba34ccd2410095daaa24
shahnoor <113544135+shahnoork109@users.noreply.github.com>
2025-01-23 05:08:35 +0000

    numbers and maths

---
+++ 01_basics/12_numbersAndMaths.js
@@ -0,0 +1,32 @@
+//  const score = 400
+//  console.log(score);
+ 
+//  const balance = new Number (1000)
+//  console.log(balance);
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+//  const otherNum = 123.8967
+//  console.log(otherNum.toPrecision(3));
+
+//  const num2 = 10000000
+//  console.log(num2.toLocaleString('en-IN'));
+ 
+//  console.log(Math);
+//  console.log(Math.abs(-4));
+//  console.log(Math.round(4.6));
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.9));
+//  console.log(Math.min(4,1,5,9,6));
+
+// console.log(Math.random());     //this gives value between 0-1
+// console.log((Math.random() * 10) +1 ); 
+// console.log( Math.floor(Math.random() * 10) +1 ); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min );
+
+ 
